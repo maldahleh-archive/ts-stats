ab2dc0de299eba8355cd898517af2e9698986803
Mohammed Al-Dahleh <maldahleh@gmail.com>
2019-06-24 22:10:01 -0400

    .gitignore fix

--- build/index.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var CsvFileReader_1 = require("./CsvFileReader");
-var MatchReader_1 = require("./MatchReader");
-var matchReader = new MatchReader_1.MatchReader(new CsvFileReader_1.CsvFileReader('football.csv'));
-console.log(matchReader.matches);
